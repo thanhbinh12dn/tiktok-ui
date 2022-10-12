a8aede56509ffccdfae2065cfda8e1f0bd51c783
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2022-10-12 15:54:32 +0700

    Call Api, get data, render data

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,23 +1,24 @@
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom"
 import classNames from "classnames/bind"
 import Image from "~/components/Image"
 import styles from './AccountItem.module.scss'
 
 const cx = classNames.bind(styles)
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9c1763d086163fc41c05a6d731057f7f~c5_300x300.webp?x-expires=1665288000&x-signature=%2Bujj4tZqB6VEJSg2dl0nvIRp3uw%3D" alt="Hoa"/>
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name}/>
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Le Phuong Hoa</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>}
                 </h4>
-                <span className={cx('username')}>daolephuonghoa</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     )
 }
 
--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -14,14 +14,28 @@ function Search() {
     const [searchValue, setSearchValue] = useState('')
     const [searchResult, setSearchResult] = useState([])
     const [showResult, setShowResult] = useState(true)
+    const [loading, setLoading] = useState(false)
 
     const inputRef = useRef()
 
     useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1, 1, 1])
-        }, 0)
-    }, [])
+        if(!searchValue.trim()) {
+            setSearchResult([])
+            return
+        }
+
+        setLoading(true)
+
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+            .then(res => res.json())
+            .then(res => {
+                setSearchResult(res.data)
+                setLoading(false)
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }, [searchValue])
 
     const handleClear = () => {
         setSearchValue('')
@@ -43,9 +57,9 @@ function Search() {
                         <h3 className={cx('search-title')}>
                             Tài khoản
                         </h3>
-                        <AccountItem />
-                        <AccountItem />
-                        <AccountItem />
+                        {searchResult.map((result) => (
+                            <AccountItem key={result.id} data={result}/>
+                        ))}
                     </PopperWrapper>
                 </div>
             )}
@@ -60,12 +74,12 @@ function Search() {
                     onChange={e => setSearchValue(e.target.value)}
                     onFocus={() => setShowResult(true)}
                 />
-                {!!searchValue && (
+                {!!searchValue && !loading && (
                     <button className={cx('clear')} onClick={handleClear}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
                 )}
-                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
                 
                 <button className={cx('search-btn')}>
                     <SearchIcon />
