1ec76a2969d3e633e2eadf7b090b23cf1c748549
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2022-10-15 21:21:49 +0700

    create instance axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -3,6 +3,8 @@ import classNames from 'classnames/bind';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+import * as searchServices from '~/apiServices/searchServices'
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons'
@@ -27,17 +29,20 @@ function Search() {
             return
         }
 
+        const fetchApi = async () => {
+            setLoading(true)
+
+            const result = await searchServices.search(debounced)
+            
+            setSearchResult(result)
+
+            setLoading(false)
+        }
+        fetchApi()
+
         setLoading(true)
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
-            .then(res => res.json())
-            .then(res => {
-                setSearchResult(res.data)
-                setLoading(false)
-            })
-            .catch(() => {
-                setLoading(false)
-            })
+        
     }, [debounced])
 
     const handleClear = () => {
