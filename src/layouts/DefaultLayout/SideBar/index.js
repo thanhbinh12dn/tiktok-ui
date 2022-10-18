af5e1286833ac0859823d21ed1956edc359c082f
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2022-10-18 10:24:59 +0700

    Solve the problem of multiple index files

--- src/layouts/DefaultLayout/SideBar/index.js
+++ src/layouts/DefaultLayout/SideBar/index.js
@@ -1,12 +1,1 @@
-import classNames from 'classnames/bind'
-import styles from './SideBar.module.scss'
-
-const cx = classNames.bind(styles)
-
-function SideBar() {
-    return <aside className={cx('wrapper')}>
-        <h2>Side Bar</h2>
-    </aside>
-}
-
-export default SideBar
+export { default } from './SideBar'
