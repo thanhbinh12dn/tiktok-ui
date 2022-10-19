9c21ddaf91df0add96566aa9873831b6c4142cc9
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2022-10-19 15:46:27 +0700

    Build UI SideBar step 2 & 3

---
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -0,0 +1,54 @@
+// import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+
+import styles from './SuggestedAccounts.module.scss'
+import { Wrapper as PopperWrapper } from '~/components/Popper'
+import AccountPreview from './AccountPreview/AccountPreview';
+
+const cx = classNames.bind(styles)
+
+function AccountItem() {
+
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview/>
+                </PopperWrapper>
+            </div>
+        )
+    }
+
+    return (
+        //Using a wrapper <div> tag around the reference element solves this by creating a new parentNode context.
+        <div>
+            <Tippy
+                interactive
+                delay={[800, 0]}
+                offset={[-20, 0]}
+                placement="bottom"
+                render={renderPreview}
+            >
+                <div className={cx('account-item')}>
+                    <img className={cx('avatar')} src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1666321200&x-signature=Zn2dGCMgpJx%2Bzq5vEdvRWMhL2uk%3D" alt="user" />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>binhthanhbui</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle}/>
+                        </p>
+                        <p className={cx('name')}>Bui Thanh Binh</p>
+                    </div>
+                </div>
+            </Tippy>
+        </div>
+    )
+}
+
+// AccountItem.propTypes = {
+
+// }
+
+export default AccountItem
--- src/layouts/components/SideBar/SideBar.js
+++ src/layouts/components/SideBar/SideBar.js
@@ -3,6 +3,7 @@ import classNames from 'classnames/bind'
 import styles from './SideBar.module.scss'
 import config from '~/config'
 import { HomeIcon, HomeActiveIcon, UserGroupIcon, UserGroupActiveIcon, LiveIcon, LiveActiveIcon } from '~/components/Icons'
+import SuggestedAccounts from '~/components/SuggestedAccounts'
 
 const cx = classNames.bind(styles)
 
@@ -12,6 +13,9 @@ function SideBar() {
             <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon/>} activeIcon={<HomeActiveIcon />}></MenuItem>
             <MenuItem title="Following" to={config.routes.following} icon={<UserGroupIcon/>} activeIcon={<UserGroupActiveIcon />}></MenuItem>
             <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon/>} activeIcon={<LiveActiveIcon />}></MenuItem>
+
+            <SuggestedAccounts label='Tài khoản được đề xuất'/>
+            <SuggestedAccounts label='Các tài khoản đang follow'/>
         </Menu>
     </aside>
 }
